ee5519b026e77def1a78a62efc4eb7845c0ca262
Talwinder Singh <singhtalwinder790@gmail.com>
2022-06-14 16:02:04 +0530

    add data

---
+++ data/projectsData.js
@@ -0,0 +1,20 @@
+const projectsData = [
+  {
+    title: 'Payever',
+    description: `Payever is a unique commerce solution that covers the whole sales cycle, from online shop and 
+    point-of-sale software to payment, customer relationship management, marketing, inventory, and shipping tools. 
+    All solutions interlock seamlessly, and when your business grows, the intuitive platform used by over 5,000 merchants 
+    just grows with you.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://getpayever.com/',
+  },
+  {
+    title: 'Yjewelry',
+    description: `Yjewelry is an online eCommerce store where you can buy from the thousands of available chains, 
+    Charms, and sets or curate your own unique design using Yjewelry's online curator.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://y.jewelry/',
+  },
+]
+
+export default projectsData
