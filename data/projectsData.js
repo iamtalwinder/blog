a70682358f82a267fce35e45fd0e3c7f90a99b39
Talwinder Singh <singhtalwinder790@gmail.com>
2022-07-21 21:06:02 +0530

    add project images

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,32 +5,32 @@ const projectsData = [
     point-of-sale software to payment, customer relationship management, marketing, inventory, and shipping tools. 
     All solutions interlock seamlessly, and when your business grows, the intuitive platform used by over 5,000 merchants 
     just grows with you.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/projects/payever.jpg',
     href: 'https://getpayever.com/',
   },
   {
     title: 'Yjewelry',
     description: `Yjewelry is an online eCommerce store where you can buy from the thousands of available chains, 
     Charms, and sets or curate your own unique design using Yjewelry's online curator.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/projects/y-jewelry.png',
     href: 'https://y.jewelry/',
   },
   {
     title: 'Shiftal',
     description: `Shiftal is an online Crypto trading platform built on top of openDAX where you can trade 50+ cryptocurrencies with ease.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/projects/shiftal.png',
     href: 'https://www.shiftal.com/',
   },
   {
     title: 'Motorsport Multiverse',
     description: `Simple and intuitive platform to Buy, collect, and Trade motorsport NFTs.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/projects/motorsport.png',
     href: 'https://frontend-dev.motorsportmultiverse.com/',
   },
   {
     title: 'Settleezee',
     description: `Settleezee simplifies the study abroad process by connecting students and recruitment partner schools on a single platform.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/projects/settleezee.png',
     href: 'https://www.settleezee.com/',
   },
 ]
